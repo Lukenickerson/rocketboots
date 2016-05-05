0469159a47d7615edd3094042fb63e5ceb8f24b3
Luke Nickerson <luke.nickerson@gmail.com>
2016-05-04 23:30:03 -0400

    extra check for NaN, clone function, round function

--- scripts/rocketboots/coords.js
+++ scripts/rocketboots/coords.js
@@ -20,13 +20,24 @@
 		if (typeof coord.x != "number") {
 			console.error("Bad coord.x", coord.x);
 			coord.x = 0;
+		} else if (isNaN(coord.x)) {
+			console.error("coord.x Not a Number", coord);
+			coord.x = 0;
 		}
 		if (typeof coord.y != "number") {
 			console.error("Bad coord.y", coord.y);
 			coord.y = 0;
+		} else if (isNaN(coord.y)) {
+			console.error("coord.y Not a Number", coord);
+			coord.y = 0;
 		}
 		return coord;
 	}
+	Coords.prototype.getCopy = function () {
+		var coord = this;
+		return new Coords(coord.x, coord.y);
+	};
+	Coords.prototype.clone = Coords.prototype.getCopy;
 	Coords.prototype.set = function(coord){
 		this.checkCoords(coord);
 		this.x = coord.x;
@@ -41,6 +52,11 @@
 	Coords.prototype.multiply = function(n){
 		this.x *= n;
 		this.y *= n;
+		return this;
+	}
+	Coords.prototype.round = function(){
+		this.x = Math.round(this.x);
+		this.y = Math.round(this.y);
 		return this;
 	}
 	Coords.prototype.getMultiply = function(n){
@@ -78,11 +94,11 @@
 		var d = Math.abs(this.getDistance(coord));
 		if (this.x != coord.x) {
 			var dx = coord.x - this.x;
-			x =  dx / d;
+			x = (d == 0) ? 0 : dx / d;
 		}
 		if (this.y != coord.y) { 
 			var dy = coord.y - this.y;
-			y = dy / d;
+			y = (d == 0) ? 0 : dy / d;
 		}
 		return new Coords(x, y);
 	}

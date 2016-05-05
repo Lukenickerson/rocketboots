af28332f52fcb36a67ebd271995c325f627ada4b
Luke Nickerson <luke.nickerson@gmail.com>
2016-05-04 23:32:19 -0400

    lots of changes

--- scripts/rocketboots/state_machine.js
+++ scripts/rocketboots/state_machine.js
@@ -12,6 +12,9 @@
 	var StateMachine = function(){
 		this.states = {};
 		this.currentState = null;
+		this.history = [];
+		this._pruneHistoryAt = 200;
+		this._pruneHistoryTo = 100;
 	}
 
 	// Getters
@@ -25,22 +28,47 @@
 			return this.states[name];
 		}
 	}
-	StateMachine.prototype.add = function(name, settingsObj){	
-		if (typeof settingsObj == "function") {
-			settingsObj = { start : settingsObj };
-		} else if (typeof settingsObj != "object") {
-			settingsObj = {};
+	StateMachine.prototype.add = function(options){
+		var name;
+		if (typeof options === "string") {
+			name = options;
+			options = {};
+		} else if (typeof options === "object") {
+			name = options.name;
 		}
-		this.states[name] = new this.State(name, settingsObj);
+		this.states[name] = new this.State(name, options);
 		return this;
-	}
-	StateMachine.prototype.transition = function(newState){
-		console.log("State Machine: Transition from " + this.currentState.name + " to " + newState);
+	};
+	StateMachine.prototype.addStates = function (obj) {
+		var sm = this;
+		$.each(obj, function(stateName, stateOptions){
+			stateOptions.name = stateName;
+			sm.add(stateOptions);
+		});
+		return sm;
+	};
+	StateMachine.prototype.transition = function(newState, recordHistory){
+		recordHistory = (typeof recordHistory === 'boolean') ? recordHistory : true;
+		console.log("State Machine: Transition from " + this.currentState.name + " to " + newState, (recordHistory ? "": "(no history)"));
 		this.currentState.end();
 		this.currentState = this.get(newState);
+		if (recordHistory) {
+			this.history.push(newState);
+			if (this.history.length > this._pruneHistoryAt) {
+				this.history.splice(0, (this._pruneHistoryAt - this._pruneHistoryTo));
+			}
+		}
 		this.currentState.start();
 		return this;
-	}
+	};
+	StateMachine.prototype.back = function() {
+		if (this.history.length >= 2) {
+			this.history.pop();
+			var end = this.history.length - 1;
+			this.transition(this.history[end], false);
+		}
+		return this;
+	};
 	StateMachine.prototype.start = function(stateName){
 		$('.state').hide();
 		this.currentState = this.get(stateName);
@@ -50,32 +78,40 @@
 	//sm.prototype.init();
 
 	//==== State Class
-	StateMachine.prototype.State = function(name, settingsObj){
+	StateMachine.prototype.State = function(name, settings){
 		this.name	= name;
-		this.$view	= $('.state.'+ name);
+		this.viewName = settings.viewName || name;
+		this.$view	= $('.state.'+ this.viewName);
 		this.start 	= null;
 		this.end 	= null;
 		this.update	= null;
-		this.type 	= settingsObj.type || null;
+		this.type 	= settings.type || null;
+		this.autoView = settings.autoView || true;
 		// Init
-		this.setStart(settingsObj.start);
-		this.setEnd(settingsObj.end);
-		this.setUpdate(settingsObj.update);
+		this.setStart(settings.start);
+		this.setEnd(settings.end);
+		this.setUpdate(settings.update);
 	}
 	// Setters
 	StateMachine.prototype.State.prototype.setStart = function(fn){
-		if (typeof fn == "function") this.start = fn;
-		else this.start = function(){
-			this.$view.show();
+		var s = this;
+		this.start = function () {
+			if (typeof fn == "function") fn.apply(s); // TODO: use call or apply?
+			if (s.autoView) {
+				s.$view.show(); 
+			}
 		};
-		return this;
+		return s;
 	}
 	StateMachine.prototype.State.prototype.setEnd = function(fn){
-		if (typeof fn == "function") this.end = fn;
-		else this.end = function(){
-			this.$view.hide();
+		var s = this;
+		this.end = function () {
+			if (typeof fn == "function") fn.apply(s); // TODO: use call or apply?
+			if (s.autoView) {
+				s.$view.hide(); 
+			}
 		};
-		return this;
+		return s;		
 	}
 	StateMachine.prototype.State.prototype.setUpdate = function(fn){
 		if (typeof fn == "function") this.update = fn;

8566c06db6a4560e5550e0bf831cd8b3308d6455
Luke Nickerson <luke.nickerson@gmail.com>
2016-05-04 23:30:54 -0400

    lots of fixes

--- scripts/rocketboots/entity.js
+++ scripts/rocketboots/entity.js
@@ -8,40 +8,44 @@
 	//=========================================================================
 	//==== Entity
 	
-	var Entity = function(name, world, size){
-		if (typeof size !== 'object') {
-			size = { x: 10, y: 10 };
+	var Entity = function(options, world, size){
+		if (typeof options === 'string') {
+			options = {name: options};
+		} else {
+			options = options || {};
 		}
-		this.name = name;
-		this.groups	= [];
-		this.groupIndices = {};
-		this.world 	= world;
-		this.stageOffset = new this.Coords(0,0); // for minor pixel offsets
-		this.pos 	= new this.Coords(0,0);
-		this.vel 	= new this.Coords(0,0);
-		this.mass	= 1;
-		this.size 	= new this.Coords(size.x, size.y);
-		this._halfSize = new this.Coords(size.x/2, size.y/2);
-		this.radius = parseInt(size.x/2);
-		this.image	= null;
-		this.color 	= "#666";
+		world = world || options.world || null;
+		size = size || options.size || { x: 10, y: 10 };
+		var pos = options.pos || { x: 0, y: 0 };
+
+		this.name 			= options.name || null;
+		this.groups			= [];
+		this.world 			= world;
+		this.stageOffset 	= new this.Coords(0,0); // for minor pixel offsets
+		this.pos 			= new this.Coords(pos.x, pos.y);
+		this.vel 			= new this.Coords(0,0);
+		this.mass			= 1;
+		this.size 			= new this.Coords(size.x, size.y);
+		this._halfSize 		= new this.Coords(size.x/2, size.y/2);
+		this.radius 		= parseInt(size.x/2);
+		this.image			= null;
+		this.color 			= options.color || "#666";
 		this.collisionShape = "circle"; // *** doesn't matter yet
 		// various on/off states
 		this.isHighlighted 	= false;
 		this.isPhysical 	= true;
 		this.isMovable		= true;
+		this.isVisible 		= true;
 		// Custom draw functions
-		this.customDraw = {
-			highlighted : null
+		this.draw = {
+			before: null,
+			custom: null,
+			highlight: null,
+			after: null
 		};
 		// Entity Can Contain Another
-		this.tags = ["all", "physical", "movable", "physics"];
-		this.contains = { 
-			"all" : [],
-			"physical" : [],
-			"movable" : [],
-			"physics" : []
-		};
+		this.entityGroups = [];
+		this.entities = { };
 	}
 	// Sets
 	Entity.prototype.setSize = function(x,y){
@@ -53,16 +57,22 @@
 	Entity.prototype.getType = function(){
 		return this.groups[0];
 	}
-	Entity.prototype.isInGroup = function(group){
+	Entity.prototype.isInGroup = function (group) {
 		return (this.groups.indexOf(group) == -1) ? false : true;
 	}
+	Entity.prototype.getHeadPos = function(){
+		return new this.Coords(this.pos.x, this.pos.y + this._halfSize.y);
+	};
+	Entity.prototype.getFootPos = function(){
+		return new this.Coords(this.pos.x, this.pos.y - this._halfSize.y);
+	};
 	
 	// Put in / take out
-	
 	Entity.prototype.putIn = function(ent, groups, isFront){
 		if (typeof groups == "string") groups = [groups];
 		if (typeof isFront != "boolean") isFront = false;
 		var grp = "", groupIndex = -1;
+		groups = groups.concat("all");
 		// Add entity to groups
 		for (var t = 0; t < groups.length; t++){
 			grp = groups[t];
@@ -75,18 +85,24 @@
 				} else {
 					ent.groups.push(grp);
 				}
-				ent.groupIndices[grp] = groupIndex;
+				//ent.groupIndices[grp] = groupIndex;
+			} else {
+				console.warn('Entity already in group', grp, ent, this.groups);
 			}
 		}
 		return ent;
-	}
-	Entity.prototype.putNewIn = function(name, groups){
+	};
+
+	Entity.prototype.putNewIn = function(name, groups, isFront){
+		if (typeof isFront != "boolean") isFront = false;
 		var ent = new Entity(name, this, this.grid.size);
 		groups = groups.concat("all");
-		ent = this.putIn(ent, groups);
+		ent = this.putIn(ent, groups, isFront);
 		this.categorizeEntitiesByGroup();
 		return ent;
-	}
+	}; 
+
+
 	Entity.prototype.addEntityGroup = function(type){
 		var typeId = this.entityGroups.indexOf(type);
 		if (typeId == -1) {
@@ -94,42 +110,74 @@
 			this.entities[type] = [];
 		}
 		return typeId;
-	}
+	};
+	Entity.prototype.addEntityGroups = function(typeArr){
+		var o = this;
+		var typeIds = [];
+		$.each(typeArr, function(i, elt){
+			var typeId = o.addEntityGroup(elt);
+			typeIds.push(typeId);
+		});
+		return typeIds;
+	};
+
+	Entity.prototype.findGroupIndex = function (group) {	// 'group' is an array of entities to look in (haystack), 'this' is the needle
+		var i = group.length;
+		while (i--) {
+			if (group[i] === this) {
+				return i;
+			}
+		}
+		return -1;
+	};
+
 	Entity.prototype.takeOut = function(ent, remGroups){
+		var remGroupName = "", remGroupIndex = -1, group;
 		//console.log("Remove groups", remGroups, typeof remGroups);
-		if (typeof remGroups == "string") remGroups = [remGroups];
-		else if (typeof remGroups == "undefined") remGroups = ["all"];
+		if (typeof remGroups == "string") { remGroups = [remGroups]; }
+		else if (typeof remGroups == "undefined") { remGroups = ["all"]; }
 		// Remove "all" groups?
 		if (remGroups.indexOf("all") != -1) {	
 			remGroups = ent.groups.join("/").split("/");
 		}
-		//console.log("Remove groups", remGroups, ent.groups);
-		var remGroup = "", remGroupIndex = -1;
+		console.log("Take", (ent.name || "entity"), "out of groups", remGroups, ent.groups);
+
 		// Loop over groups to remove
 		for (var g = 0; g < remGroups.length; g++){
-			remGroup = remGroups[g];
-			if (ent.isInGroup(remGroup)) {
+			remGroupName = remGroups[g];
+			if (ent.isInGroup(remGroupName)) {
 				
-				remGroupIndex = ent.groupIndices[remGroup];
-				//console.log("Removing", remGroup, remGroupIndex);
+				group = this.entities[remGroupName];
+
+				remGroupIndex = ent.findGroupIndex(group);
+				
 				// Remove from group array
-				//this.entities[remGroup].splice(remGroupIndex, 1);
-				// ^ can't splice this out or all the indices get messed up
-				this.entities[remGroup][remGroupIndex] = null;
-				// *** ^ This might cause memory issues??
+				//group[remGroupIndex] = null;
+				group.splice(remGroupIndex, 1);
+
 				// Remove from entity's properties
-				ent.groups.splice( ent.groups.indexOf(remGroup), 1 );
-				delete ent.groupIndices[remGroup];
+				ent.groups.splice( ent.groups.indexOf(remGroupName), 1 );
 			}
 		}
 		return ent;
-	}
+	};
 
-	// Bring in a pointer to the Coords class from RocketBoots
-	Entity.prototype.Coords = (typeof RocketBoots.Coords == "function") ? RocketBoots.Coords : Coords;
+	Entity.prototype.isIntersecting = function (ent) {
+		// ***
+	};
+
+	// Aliases
+	Entity.prototype.addEntity = Entity.prototype.putIn; 
+	Entity.prototype.addNewEntity = Entity.prototype.putNewIn;	
+	Entity.prototype.removeEntity = Entity.prototype.takeOut;
+
+	function initEntityPrototype () {
+		// Bring in a pointer to the Coords class from RocketBoots
+		Entity.prototype.Coords = (typeof RocketBoots.Coords == "function") ? RocketBoots.Coords : Coords;
+	}
 	
 	// Install as RocketBoots component
 	if (typeof RocketBoots == "object") {
-		RocketBoots.installComponent("entity", "Entity", Entity);
+		RocketBoots.installComponent("entity", "Entity", Entity, ["Coords"], initEntityPrototype);
 	} else window.Entity = Entity;
 })();
